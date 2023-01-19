8fa7989e77fbee68d2de3ffc0210f352f8d74969
ismailcubuk <iismailcubuk@gmail.com>
2023-01-20 01:50:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRight, faL } from '@fortawesome/free-solid-svg-icons'
 import { faCopy } from '@fortawesome/free-regular-svg-icons'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [password, setPassword] = useState("asdasdas")
+  const [password, setPassword] = useState("")
   const [characterLength, setCharacterLength] = useState("5")
 
+  const [LowerText, setLowerText] = useState("")
+  const [UpperText, setUpperText] = useState("")
+  const [NumericText, setNumericText] = useState("")
+
+  const [activeUpper, setActiveUpper] = useState(false)
+  const [activeLower, setActiveLower] = useState(false)
+  const [activeNumeric, setActiveNumeric] = useState(false)
+  // validate+
+  const Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const Lower = "abcdefghijklmnopqrstuvwxyz";
+  const Numbers = "1234567890";
+  const Symbols = ".!#+%?";
+  // validate-
+
+  // UPPER
+  const UpperClick = (e) => {
+    setActiveUpper(e.target.value)
+    setActiveUpper(!activeUpper)
+  }
+  useEffect(() => {
+    if (activeUpper === true) {
+      setUpperText(Upper)
+    } else {
+      setUpperText()
+    }
+  }, [activeUpper])
+  // LOWER
+  const LowerClick = (e) => {
+    setActiveLower(e.target.value)
+    setActiveLower(!activeLower)
+  }
+  useEffect(() => {
+    if (activeLower === true) {
+      setLowerText(Lower)
+    } else {
+      setLowerText()
+    }
+  }, [activeLower])
+  // numeric
+  const NumberClick = (e) => {
+    setActiveNumeric(e.target.value)
+    setActiveNumeric(!activeNumeric)
+  }
+  useEffect(() => {
+    if (activeNumeric === true) {
+      return setNumericText(Numbers)
+    } else {
+      return setNumericText()
+    }
+  }, [activeNumeric])
+  const SymbolsClick = () => {
+  }
+  const sifre = UpperText + LowerText + NumericText
+  console.log(sifre);
+
+
   const handleClick = () => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
     let letters = []
     for (let i = 0; i < characterLength; i++) {
-      const randomIndex = Math.floor(Math.random() * alphabet.length);
-      letters.push(alphabet[randomIndex])
+      const randomIndex = Math.floor(Math.random() * sifre.length);
+      letters.push(sifre[randomIndex])
     }
     setPassword(letters);
   }
+
   const handleChange = (e) => {
     setCharacterLength(e.target.value)
   }
@@ -43,19 +99,40 @@ function App() {
           {/* OPTİONS SELECT */}
           <div className="options">
             <div>
-              <input type="checkbox" className="checkbox" name="vehicle1" value="Bike" />
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={(e) => UpperClick(e)}
+                value={activeUpper}
+              />
               <h3>Include Uppercase Letters</h3>
             </div>
+
+            {/* BURASI */}
             <div>
-              <input type="checkbox" className="checkbox" name="vehicle1" value="Bike" />
-              <h3>Include Uppercase Letters</h3>
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={(e) => LowerClick(e)}
+                value={activeLower}
+              />
+              <h3>Include Lowercase Letters</h3>
             </div>
             <div>
-              <input type="checkbox" className="checkbox" name="vehicle1" value="Bike" />
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={NumberClick}
+                value={activeNumeric}
+              />
               <h3>Include Numbers</h3>
             </div>
             <div>
-              <input type="checkbox" className="checkbox" name="vehicle1" value="Bike" />
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={SymbolsClick}
+                value="Bike" />
               <h3>Include Symbols</h3>
             </div>
           </div>
@@ -72,12 +149,12 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="generate" onClick={handleClick}>
+          {/* DİSABLED EKLE */}
+          <button className="generate" onClick={handleClick}>
             GENERATE
             <FontAwesomeIcon icon={faArrowRight} className='icon' />
-          </div>
+          </button>
         </div>
-
       </div>
     </div>
   );
