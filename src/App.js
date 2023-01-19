be12ff1848d07a4ce6d5263652e4c378f6d539a2
ismailcubuk <iismailcubuk@gmail.com>
2023-01-20 01:50:15 +0300

    test

--- src/App.js
+++ src/App.js
@@ -32,7 +32,14 @@ function App() {
             <h3>Character Length</h3>
             <h1>{characterLength}</h1>
           </div>
-          <input type="range" id="range" name="range" min={1} max={10} value={characterLength} onChange={handleChange} />
+          <input
+            type="range"
+            id="range"
+            name="range"
+            min={1} max={10} step={1}
+            value={characterLength}
+            onChange={handleChange}
+          />
           {/* OPTİONS SELECT */}
           <div className="options">
             <div>
