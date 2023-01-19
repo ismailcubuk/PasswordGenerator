dbeab8913dabab2536961f3685d8ec79fe5602df
ismailcubuk <iismailcubuk@gmail.com>
2023-01-19 17:11:25 +0300

    next commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faCopy } from '@fortawesome/free-regular-svg-icons'
+import { useState } from 'react';
 
 function App() {
+  const [password, setPassword] = useState("asdasdas")
+  const [characterLength, setCharacterLength] = useState("5")
+
+  const handleClick = () => {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let letters = []
+    for (let i = 0; i < characterLength; i++) {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      letters.push(alphabet[randomIndex])
+    }
+    setPassword(letters);
+  }
+  const handleChange = (e) => {
+    setCharacterLength(e.target.value)
+  }
   return (
     <div className="background">
       <div className="border">
         <h3 className="header">Password Generator</h3>
         <div className="generate-password">
-          <h2>assdadasd15625</h2>
+          <h2>{password}</h2>
           <FontAwesomeIcon icon={faCopy} className='copy-icon' />
         </div>
         <div className="generate-area">
           <div className="character-length">
             <h3>Character Length</h3>
-            <h1>10</h1>
+            <h1>{characterLength}</h1>
           </div>
-          <input type="range" id="range" name="range" value="50" />
+          <input type="range" id="range" name="range" min={1} max={10} value={characterLength} onChange={handleChange} />
           {/* OPTİONS SELECT */}
           <div className="options">
             <div>
@@ -49,7 +65,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="generate">
+          <div className="generate" onClick={handleClick}>
             GENERATE
             <FontAwesomeIcon icon={faArrowRight} className='icon' />
           </div>
