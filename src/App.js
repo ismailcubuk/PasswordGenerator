f97bbecfdf3e19d5155294c073d2160ecf01ec79
ismailcubuk <iismailcubuk@gmail.com>
2023-01-19 12:26:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="border">
+        <h3 className="header">Password Generator</h3>
+        <div>
+          <h2>assdadasd15625</h2>
+          <h2>CPY</h2>
+        </div>
+        <div className="generate-area">
+          <div>
+            <h3>Character Length</h3>
+            <h1>10</h1>
+          </div>
+          <div>__________________</div>
+          <div className="options">
+            <div>
+              <div>check box</div>
+              <h3>Include Uppercase Letters</h3>
+            </div>
+            <div>
+              <div>check box</div>
+              <h3>Include Uppercase Letters</h3>
+            </div>
+            <div>
+              <div>check box</div>
+              <h3>Include Numbers</h3>
+            </div>
+            <div>
+              <div>check box</div>
+              <h3>Include Symbols</h3>
+            </div>
+          </div>
+          <div>
+            <h3>STRENTH</h3>
+            <div>
+              <h2>MEDIUM</h2>
+              <div>I I I I</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            GENERATE
+          </div>
+          <div>
+         ->
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
